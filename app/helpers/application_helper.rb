c36097d92d3292b3a9f53e14caf0abe6328a6d20
Ayra Obazee <>
2020-09-18 15:56:42 +0800

    [contacts] create contact scaffold

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def flash_messages(*)
+    flash.each do |msg_type, message|
+      concat(
+        content_tag(:div, message.html_safe,
+                    role: "alert",
+                    class: "notification #{bootstrap_class_for(msg_type)}") do
+          concat(message.html_safe)
+        end
+      )
+      flash.clear
+    end
+    nil
+  end
+
+  def bootstrap_class_for(flash_type)
+    {
+      success: "text-success",
+      error: "text-danger",
+      alert: "text-warning",
+      notice: "text-info"
+    }[flash_type.to_sym]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_045128) do
+ActiveRecord::Schema.define(version: 2020_09_18_073439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "mobile"
+    t.string "gender"
+    t.string "street_address"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.string "zip_code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
